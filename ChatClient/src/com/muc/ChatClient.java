15bef1fa946f0a1651a4815cb8782fa1345ae9d4
bindzo <51052760+bindzo@users.noreply.github.com>
2020-07-18 22:28:54 +0700

    Tạo người dùng

---
+++ ChatClient/src/com/muc/ChatClient.java
@@ -0,0 +1,59 @@
+package com.muc;
+
+import java.io.*;
+import java.net.Socket;
+
+public class ChatClient {
+    private final String serverName;
+    private final int serverPort;
+    private Socket socket;
+    private InputStream serverIn;
+    private OutputStream serverOut;
+    private BufferedReader bufferedIn;
+
+    public ChatClient(String serverName, int serverPort) {
+        this.serverName = serverName;
+        this.serverPort = serverPort;
+    }
+
+    public static void main(String[] args) throws IOException {
+        ChatClient client = new ChatClient("localhost",8818);
+        if(!client.connect()){
+            System.err.println("Connect failed.");
+        }else{
+            System.out.println("Connect successful");
+            if(client.login("a","a")){
+                System.out.println("Login successful");
+            }else{
+                System.err.println("Login failed");
+            }
+        }
+    }
+
+    private boolean login(String login, String password) throws IOException {
+        String cmd = "login " + login + " " + password +"\n";
+        serverOut.write(cmd.getBytes());
+
+        String response = bufferedIn.readLine();
+        System.out.println("Response Line: "+response);
+        if("ok login".equalsIgnoreCase(response))
+        {
+            return true;
+        }else
+            return false;
+    }
+
+    private boolean connect() {
+        try {
+            this.socket = new Socket(serverName, serverPort);
+            this.serverOut = socket.getOutputStream();
+            this.serverIn = socket.getInputStream();
+            this.bufferedIn = new BufferedReader(new InputStreamReader(serverIn));
+
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
